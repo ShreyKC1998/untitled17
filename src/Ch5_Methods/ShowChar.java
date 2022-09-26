aec200f30829924ca906f0cce01de4d82d611eea
noobmaster69 <cjaguirre1988@gmail.com>
2022-09-26 01:45:11 -0400

    new commit

--- src/Ch5_Methods/ShowChar.java
+++ src/Ch5_Methods/ShowChar.java
@@ -1,5 +1,21 @@
 package Ch5_Methods;
 
 public class ShowChar {
-    
+    public static void main(String[] args) {
+
+        System.out.println(showChar("New York", 2));
+    }
+
+    public static char showChar(String obj, int pos){
+
+        char position = 0;
+        for (int i = 0; i < obj.length(); i++) {
+            pos = i;
+
+        }
+        return position;
+    }
+
 }
+
+
